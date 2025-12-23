819860b9367a8523687da1fdac0d0a6ee21755c5
sneha931 <cs22157@velammalitech.edu.in>
2025-12-24 00:36:34 +0530

    http requests

--- src/main/java/com/example/MyWebApp/Controller/ProductController.java
+++ src/main/java/com/example/MyWebApp/Controller/ProductController.java
@@ -4,9 +4,7 @@ package com.example.MyWebApp.Controller;
 import com.example.MyWebApp.Model.Product;
 import com.example.MyWebApp.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -16,13 +14,26 @@ public class ProductController {
     @Autowired
 
     ProductService service;
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
 
         return service.getProducts();
     }
-    @RequestMapping("/product/{prodid}")
+    @GetMapping ("/product/{prodid}")
     public Product getproductbyId(@PathVariable  int prodid){
         return  service.getproductbyId(prodid);
+    }
+
+    @PostMapping("/products")
+    public void addproduct(@RequestBody  Product prod){
+        service.addproduct(prod);
+    }
+    @PutMapping("/products")
+    public void updateproduct(@RequestBody Product prod){
+        service.updateproduct(prod);
+    }
+    @DeleteMapping("/products/{prodid}")
+    public void deleteproduct(@PathVariable int prodid){
+        service.deleteproduct(prodid);
     }
 }
--- src/main/java/com/example/MyWebApp/Service/ProductService.java
+++ src/main/java/com/example/MyWebApp/Service/ProductService.java
@@ -3,13 +3,14 @@ package com.example.MyWebApp.Service;
 import com.example.MyWebApp.Model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
 @Service
 public class ProductService {
-    List<Product> products=Arrays.asList(new Product(100,"laptop",60000),
-            new Product(101,"mobile",80000));
+    List<Product> products=new ArrayList<>(Arrays.asList(new Product(100,"laptop",60000),
+            new Product(101,"mobile",80000)));
      public List<Product> getProducts(){
         return products;
 
@@ -17,5 +18,19 @@ public class ProductService {
     public Product getproductbyId(int prodid){
          return products.stream().filter(p->p.getProdId()==prodid).findFirst().get();
 
+    }
+    public void addproduct(Product prod){
+         products.add(prod);
+    }
+    public void  updateproduct(Product prod){
+         int idx=0;
+         for(int i=0;i<products.size();i++){
+             if(products.get(i).getProdId()==prod.getProdId())
+                 idx=i;
+         }
+        products.set(idx, prod);
+    }
+    public void  deleteproduct(int prodid){
+         products.removeIf(p->p.getProdId()==prodid);
     }
 }
